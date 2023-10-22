c4a0ea58d1b04d1fd1c5ee4fb88a3366dac942a8
giov-prl <86360226+giov-prl@users.noreply.github.com>
2023-10-22 11:12:02 +0200

    Update MyClassTest.java

--- MyClass/MyClassTest.java
+++ MyClass/MyClassTest.java
@@ -6,32 +6,50 @@ public class MyClassTest {
 
     @Test
     public void testAdd() {
+    	assertEquals(4, myClass.add(2,2);
         assertEquals(5, myClass.add(2, 3));
     }
 
     @Test
     public void testSubtract() {
-        assertEquals(2, myClass.subtract(5, 3));
+    	int a = 5, b = 3, s = 2;
+    	if(a > b)
+         assertEquals(2, myClass.subtract(a, b));
+         assertEquals(s,s);
+         System.out.print(s);
+        else
+         assertEquals(2, myClass.subtract(b, a));
     }
 
     @Test
     public void testMultiply() {
+    	assertEquals(8, myClass.add(5,3));
         assertEquals(15, myClass.multiply(5, 3));
     }
 
     @Test
     public void testDivide() {
-        assertEquals(2.0, myClass.divide(10.0, 5.0), 0.001);
+    	try{
+    	
+         assertEquals(2.0, myClass.divide(10.0, 5.0), 0.001);
+         }
+         catch (IllegalArgumentException e){
+          Assert.fail(e.getMessage());
+         }
     }
 
     @Test
     public void testConcatenateStrings() {
-        assertEquals("Hello, World", myClass.concatenateStrings("Hello, ", "World"));
+    	String s = "Hello, World";
+        assertEquals(s, myClass.concatenateStrings("Hello, ", "World"));
+        s = "Hello, 5";
+        assertEquals(s, myClass.concatenateStrings("Hello, ", Integer(5).toString());
     }
 
     @Test
     public void testIsEven() {
         assertTrue(myClass.isEven(4));
+        Thread.sleep(5); 
         assertFalse(myClass.isEven(7));
     }
 
